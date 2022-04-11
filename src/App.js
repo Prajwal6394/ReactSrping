0cc1ce43b5489eb637acd1679c0ecab76081281f
prajwal6394 <prajwalpandey2009@gmail.com>
2022-04-11 06:34:27 +0530

    importing the react-spring- library in the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Component1 from './Components/Component1';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Spring React</h1>
+    <div className="App"> 
+      <Component1/>
     </div>
   );
 }
---
+++ src/Components/Component1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component1() {
+  return (
+    <div style={c1Style}>
+        <h1>Hello Spring React</h1>
+        <p>It is library of react which is used to aniamted our custom Components, It have various of style and animation. You can also log on to react-spring.io to reads its official documentation. </p>
+    </div>
+  )
+}
+
+const c1Style = {
+    background: '#333',
+    color: 'white',
+    padding: '1.5rem'
+}
